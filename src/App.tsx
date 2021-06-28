4aabac72b827cc250f7f57b0f769b266535e9fe5
Sviatoslav <sv_dubov@ukr.net>
2021-06-28 17:00:10 +0300

    Add template and made some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Menu from './components/Menu';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="container-fluid">
+        <div className="row">
+          <Menu />
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <Dashboard />
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+            <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+
+            <ul className="my-2 my-md-0 mr-md-3">
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
