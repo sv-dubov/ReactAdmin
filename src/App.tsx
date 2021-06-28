37533a47c7570c9e5358651cc78d6671647b2e8b
Sviatoslav <sv_dubov@ukr.net>
2021-06-28 20:03:43 +0300

    Add react-roure-dom and made some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './components/Dashboard';
-import Menu from './components/Menu';
-import Nav from './components/Nav';
+import Dashboard from './secure/Dashboard';
+import Menu from './secure/components/Menu';
+import Nav from './secure/components/Nav';
+import Users from './secure/Users';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
         <div className="row">
           <Menu />
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <Dashboard />
+            <BrowserRouter>
+              <Route path={'/'} exact component={Dashboard} />
+              <Route path={'/users'} component={Users} />
+            </BrowserRouter>
           </main>
         </div>
       </div>
---
+++ src/secure/Users.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Users = () => {
+    return (
+        <p>Users work!</p>        
+    )
+}
+
+export default Users;
