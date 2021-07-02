2a4b9a703e23557d63526a2ba3cb89090d927187
Sviatoslav <sv_dubov@ukr.net>
2021-07-02 16:30:05 +0300

    Made Role and permissions

--- src/secure/users/Users.tsx
+++ src/secure/users/Users.tsx
@@ -37,7 +37,7 @@ class Users extends Component {
                                             <td>{user.id}</td>
                                             <td>{user.first_name} {user.last_name}</td>
                                             <td>{user.email}</td>
-                                            <td></td>
+                                            <td>{user.role.name}</td>
                                             <td></td>
                                         </tr>
                                     )
