e41badc2cc3f71cd97a9a5259b3c5bc4fe8409ed
Sviatoslav <sv_dubov@ukr.net>
2021-07-06 19:44:30 +0300

    Made chart on Dashboard

--- src/secure/dashboard/Dashboard.tsx
+++ src/secure/dashboard/Dashboard.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Wrapper from '../Wrapper';
+import c3 from 'c3';
+import axios from 'axios';
 
-const Dashboard = () => {
-    return (
-        <Wrapper>
-            Dashboard
-        </Wrapper>
-    )
+class Dashboard extends Component {
+    componentDidMount = async () => {
+        let chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                x: 'x',
+                columns: [
+                    ['x'],
+                    ['Sales'],
+                ],
+                types: {
+                    Sales: 'bar'
+                }
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                }
+            }
+        });
+
+        const response = await axios.get('chart');
+        const records: { date: string, sum: number}[] = response.data.data;
+        chart.load({
+            columns: [
+                ['x', ...records.map((r: any) => r.date)],
+                ['Sales', ...records.map((r: any) => r.sum)]
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <h2>Daily sales</h2>
+
+                <div id="chart" />
+            </Wrapper>
+        )
+    }
 }
 
 export default Dashboard;
